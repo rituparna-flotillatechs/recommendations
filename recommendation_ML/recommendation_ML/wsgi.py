1a447f399d95cacd4e708b8bfe69771d5b50ef25
Ritu <mail.rituparna1980@gmail.com>
2021-12-15 01:18:20 +0530

    wsgi modified

--- recommendation_ML/recommendation_ML/wsgi.py
+++ recommendation_ML/recommendation_ML/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recommendation_ML.settings')
 application = get_wsgi_application()
 
 # ML registry
